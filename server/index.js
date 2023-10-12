22af80302c0d9ad8f6deee8dff421759fa811187
hisham <hishammpsn@gmail.com>
2023-10-12 23:50:29 +0530

    added login and reg api

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
+import cors from 'cors';
+
+import userRoutes from './Routes/userRoutes.js'
 
 dotenv.config();
+
 const app = express();
+app.use(express.json()); 
+
+app.use(cors())
+
 connectDB();
+
+app.use('/api/users', userRoutes)
+
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
