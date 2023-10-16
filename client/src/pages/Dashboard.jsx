c1986a1279186d6a65a4b1e68f7fad6a185968bc
hisham <hishammpsn@gmail.com>
2023-10-17 00:17:31 +0530

    add register api

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,23 +1,23 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
-import { tokens } from "../theme";
-import { mockTransactions } from "../data/mockData";
-import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import EmailIcon from "@mui/icons-material/Email";
-import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import TrafficIcon from "@mui/icons-material/Traffic";
-import Header from "../components/Header";
+import { Box, Button, IconButton, Typography, useTheme } from '@mui/material'
+import { tokens } from '../theme'
+import { mockTransactions } from '../data/mockData'
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined'
+import EmailIcon from '@mui/icons-material/Email'
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import TrafficIcon from '@mui/icons-material/Traffic'
+import Header from '../components/Header'
 // import LineChart from "../../components/LineChart";
 // import GeographyChart from "../../components/GeographyChart";
 // import BarChart from "../../components/BarChart";
-import StatBox from "../components/StatBox";
-import ProgressCircle from "../components/ProgressCirlcle";
-import LineChart from "../components/LineChart";
-import BarChart from "../components/BarChart";
+import StatBox from '../components/StatBox'
+import ProgressCircle from '../components/ProgressCirlcle'
+import LineChart from '../components/LineChart'
+import BarChart from '../components/BarChart'
 
 const Dashboard = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
 
   return (
     <Box m="20px">
@@ -30,12 +30,12 @@ const Dashboard = () => {
             sx={{
               backgroundColor: colors.blueAccent[700],
               color: colors.grey[100],
-              fontSize: "14px",
-              fontWeight: "bold",
-              padding: "10px 20px",
+              fontSize: '14px',
+              fontWeight: 'bold',
+              padding: '10px 20px',
             }}
           >
-            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            <DownloadOutlinedIcon sx={{ mr: '10px' }} />
             Download Reports
           </Button>
         </Box>
@@ -65,7 +65,7 @@ const Dashboard = () => {
             increase="+14%"
             icon={
               <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                sx={{ color: colors.greenAccent[600], fontSize: '26px' }}
               />
             }
           />
@@ -84,7 +84,7 @@ const Dashboard = () => {
             increase="+21%"
             icon={
               <PointOfSaleIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                sx={{ color: colors.greenAccent[600], fontSize: '26px' }}
               />
             }
           />
@@ -103,7 +103,7 @@ const Dashboard = () => {
             increase="+5%"
             icon={
               <PersonAddIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                sx={{ color: colors.greenAccent[600], fontSize: '26px' }}
               />
             }
           />
@@ -122,7 +122,7 @@ const Dashboard = () => {
             increase="+43%"
             icon={
               <TrafficIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                sx={{ color: colors.greenAccent[600], fontSize: '26px' }}
               />
             }
           />
@@ -160,7 +160,7 @@ const Dashboard = () => {
             <Box>
               <IconButton>
                 <DownloadOutlinedIcon
-                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                  sx={{ fontSize: '26px', color: colors.greenAccent[500] }}
                 />
               </IconButton>
             </Box>
@@ -280,10 +280,9 @@ const Dashboard = () => {
             <GeographyChart isDashboard={true} />
           </Box>
         </Box> */}
-
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
